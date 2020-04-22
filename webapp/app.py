f7d86709288c84fa45fa33a80b14c0eac424279f
Diego448 <diegosg448@gmail.com>
2020-04-22 14:12:57 -0500

    Minor fixes

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/student_control')
-def student_control():
+def student_control(students={}):
     return render_template("student_control.html")

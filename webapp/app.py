eff1282b8c7d284e9a4934ee6299ae0bf9208bf2
Diego448 <diegosg448@gmail.com>
2020-04-22 14:18:36 -0500

    Adding function for get student list

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+import json, requests
 app = Flask(__name__)
+
+def get_students():
+    r = requests.get('http://127.0.0.1:5000/students')
+    return r.json()
 
 @app.route('/student_control')
 def student_control(students={}):
-    return render_template("student_control.html")
+    students_data = get_students()
+    return render_template("student_control.html", students=students_data['_items'])

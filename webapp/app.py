b2930cdfb146cfac06e816f38103bfd557792164
Sorahar <sorahar.xd@gmail.com>
2020-04-22 14:10:47 -0500

    Defined template for student_control

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/student_control')
+def student_control():
+    return render_template("student_control.html")

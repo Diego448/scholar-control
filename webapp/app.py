d36e8185240d63895e2feff1c33602bf9ce93766
Sorahar <sorahar.xd@gmail.com>
2020-04-22 17:48:54 -0500

    Rename method student_control to student_directory

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ def get_students():
     r = requests.get('http://127.0.0.1:5000/students')
     return r.json()
 
-@app.route('/student_control')
-def student_control(students={}):
+@app.route('/directory/students')
+def student_directory(students={}):
     students_data = get_students()
-    return render_template("student_control.html", students=students_data['_items'])
+    return render_template("student_directory.html", students=students_data['_items'])

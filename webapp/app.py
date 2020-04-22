37326c44cfaa81a9c5e2ab14f2a9f2925d46568b
Diego448 <diegosg448@gmail.com>
2020-04-22 18:06:35 -0500

    Adding function to show add student page

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json, requests
 app = Flask(__name__)
 
@@ -10,3 +10,12 @@ def get_students():
 def student_directory(students={}):
     students_data = get_students()
     return render_template("student_directory.html", students=students_data['_items'])
+
+@app.route('/add/student', methods=['GET', 'POST'])
+def create_student():
+    if request.method == 'POST':
+        new_student = {'firstname': request.form['firstname'], 'lastname': request.form['lastname'],
+        'location': request.form['location'], 'phone_number': request.form['phone_number'],
+        'status': request.form['status']}
+        return None
+    return render_template("add_student.html")

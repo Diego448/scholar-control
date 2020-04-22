c53cda27e28b892eb9e0d15310058a9ffef2dd35
Sorahar <sorahar.xd@gmail.com>
2020-04-22 12:58:45 -0500

    Added studentes_schema to settings.py

--- restapi/settings.py
+++ restapi/settings.py
@@ -4,3 +4,28 @@ MONGO_DBNAME = 'scholar-control'
 
 RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+students_schema = {
+    'firstname': {
+        'type': 'string', 
+        'minlength': 3,
+        'required': True
+    },
+    'lastname': {
+        'type': 'string', 
+        'minlength': 3,
+        'required': True
+    }
+    'location': {
+        'type': 'string',
+        'required' = True
+    },
+    'phone_number': {
+        'type': 'string',
+        'minlength': 10
+        'maxlength': 10
+    },
+    'status': {
+        'type': 'boolean'
+    }
+}

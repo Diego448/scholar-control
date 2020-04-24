1c3212ef2e565230cdf1a90a4f1917df4f91d104
Sorahar <sorahar.xd@gmail.com>
2020-04-24 17:23:07 -0500

    Created courses schema.

--- restapi/settings.py
+++ restapi/settings.py
@@ -30,11 +30,42 @@ student_schema = {
     }
 }
 
+course_schema {
+    'name': {
+        'type': 'string',
+        'required': True
+    },
+    'tacher': {
+        'type': 'string',
+        'required': True
+    },
+    'students':{
+        'type': 'list'
+    },
+    'start': {
+        'type': 'datetime',
+        'required': True
+    },
+    'end': {
+        'type': 'datetime'
+    },
+    'status': {
+        'type': 'string'
+        'allowed': ['Programado', 'Iniciado', 'Terminado', 'Cancelado']
+    }
+}
+
 students = {
     'item_title': 'student',
     'schema': student_schema
 }
 
+courses = {
+    'item_title': 'course',
+    'schema': course_schema
+}
+
 DOMAIN = {
-    'students': students
+    'students': students,
+    'courses': courses
 }
